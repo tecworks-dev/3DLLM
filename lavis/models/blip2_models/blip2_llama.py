01c5fb9a07048453ff0266d88da0b08023be2f9f
Diantao Tu <a743742992@gmail.com>
2023-04-20 15:38:40 +0800

    增加带模板的推理

--- lavis/models/blip2_models/blip2_llama.py
+++ lavis/models/blip2_models/blip2_llama.py
@@ -17,8 +17,8 @@ import torch.nn as nn
 from lavis.common.registry import registry
 from lavis.models.blip2_models.blip2 import Blip2Base, disabled_train
 from transformers import AutoTokenizer, AutoModelForCausalLM
-import lavis.models.blip2_models.llama as llama
-# from transformers import LlamaConfig, LlamaModel, LlamaForCausalLM
+# import lavis.models.blip2_models.llama as llama
+from transformers import LlamaConfig, LlamaModel, LlamaForCausalLM
 import time
 
 @registry.register_model("blip2_llama")
@@ -48,7 +48,7 @@ class Blip2Llama(Blip2Base):
     ):
         super().__init__()
 
-        self.tokenizer = self.init_tokenizer()
+        # self.tokenizer = self.init_tokenizer()
 
         
         self.cloud_encoder, self.ln_cloud = self.init_cloud_encoder(
@@ -61,9 +61,10 @@ class Blip2Llama(Blip2Base):
             self.cloud_encoder.train = disabled_train
             logging.info("freeze point cloud encoder")
 
+        self.num_query_token = num_query_token
         # TODO : 不要固定为384 需要根据point cloud encoder的输出来确定
         self.Qformer, self.query_tokens = self.init_Qformer(
-            num_query_token, 384, encoder_layer=qformer_encoder_layer
+            self.num_query_token, self.cloud_encoder.enc_channels[-1], encoder_layer=qformer_encoder_layer
         )
 
         # 把一部分网络固定删去了, 不理解为什么
@@ -75,11 +76,15 @@ class Blip2Llama(Blip2Base):
             layer.intermediate = None
 
         t1 = time.time()
-        config = llama.LLaMAConfig.from_pretrained(llama_model_path)
-        self.llama_tokenizer = llama.LLaMATokenizer.from_pretrained(llama_model_path)
-        self.llama_model = llama.LLaMAForCausalLM.from_pretrained(llama_model_path, torch_dtype=torch.float16, config=config, state_dict=None)
-        # self.llama_tokenizer = AutoTokenizer.from_pretrained(llama_model_path, use_fast=False)
-        # self.llama_model = LlamaForCausalLM.from_pretrained(llama_model_path, torch_dtype=torch.float16)
+        # 张老师的学生提供的 llama 读取代码
+        # config = llama.LLaMAConfig.from_pretrained(llama_model_path)
+        # self.llama_tokenizer = llama.LLaMATokenizer.from_pretrained(llama_model_path)
+        # self.llama_model = llama.LLaMAForCausalLM.from_pretrained(llama_model_path, torch_dtype=torch.float16, config=config, state_dict=None)
+        # transformers 的读取代码
+        self.llama_tokenizer = AutoTokenizer.from_pretrained(llama_model_path, use_fast=False)
+        self.llama_model = LlamaForCausalLM.from_pretrained(llama_model_path, torch_dtype=torch.float16)
+        # 这里设置为True, 是为了避免 model.generate() 函数报错, 不知道为什么要这么做
+        self.llama_model.config.use_cache = True
         # self.llama_model = None
         logging.info("load llama model spend time: {:.4f} s".format(time.time() - t1))
          
@@ -89,18 +94,30 @@ class Blip2Llama(Blip2Base):
         # 给的 llama-Chinese 模型中使用的是 </s> 作为结束符
         self.eos_token_id = self.llama_tokenizer("</s>", add_special_tokens=False).input_ids[0]
 
-        # self.opt_proj = nn.Linear(
-        #     self.Qformer.config.hidden_size, self.llama_model.config.hidden_size
-        # )
+        # 增加 pad token 不然二阶段会报错 ValueError: Asking to pad but the tokenizer does not have a padding token.
+        self.llama_tokenizer.pad_token = self.llama_tokenizer.eos_token 
 
-        self.opt_proj = nn.Linear(self.Qformer.config.hidden_size, 
+        self.llama_proj = nn.Linear(self.Qformer.config.hidden_size, 
                                   self.llama_model.config.hidden_size if self.llama_model is not None else 5120 
         )
 
         self.max_txt_len = max_txt_len
-        self.prompt = prompt
-        prompt_tokens = self.llama_tokenizer(self.prompt, return_tensors="pt")
-        self.prompt_length = prompt_tokens.attention_mask.sum(1)
+        # 对预先设定的prompt进行一些处理，最终 forward 时用的输入是 query + prompt + input_text(mask 掉)
+        self.prompt = "请描述一下这个三维点云。"
+        self.prompt_tokens = self.llama_tokenizer(self.prompt, return_tensors="pt")
+        # prompt_token 会自动加一个 bos token, 所以这里要减去1
+        input_ids = self.prompt_tokens.input_ids[:, 1:]
+        self.prompt_attention_mask = self.prompt_tokens.attention_mask[:, 1:]
+        self.prompt_embedings = self.llama_model.model.embed_tokens(input_ids)
+
+        assert self.prompt_attention_mask.shape[1] == self.prompt_embedings.shape[1],  \
+                "prompt_attention_mask.shape : {} prompt_embedings.shape : {}".format(self.prompt_attention_mask.shape, self.prompt_embedings.shape)
+        self.prompt_length = self.prompt_attention_mask.sum(1)
+        logging.info("prompt {}, prompt_length : {}".format(self.prompt, self.prompt_length))
+
+        # self.prompt = prompt
+        # prompt_tokens = self.llama_tokenizer(self.prompt, return_tensors="pt")
+        # self.prompt_length = prompt_tokens.attention_mask.sum(1)
 
     def forward(self, samples):
         image = samples["cloud"]
@@ -113,6 +130,7 @@ class Blip2Llama(Blip2Base):
         image_atts = torch.ones(image_embeds.size()[:-1], dtype=torch.long).to(device)
 
         query_tokens = self.query_tokens.expand(image_embeds.shape[0], -1, -1)
+        # 这一行就相当于是把 learnable query 和 image 特征进行了交叉注意力
         query_output = self.Qformer.bert(
             query_embeds=query_tokens,
             encoder_hidden_states=image_embeds,
@@ -120,12 +138,14 @@ class Blip2Llama(Blip2Base):
             return_dict=True,
         )
 
-        inputs_opt = self.opt_proj(query_output.last_hidden_state)
-        atts_opt = torch.ones(inputs_opt.size()[:-1], dtype=torch.long).to(device)
+        # query_output.last_hidden_state [B , 32, 768] -> [B, 32, 5120]
+        inputs_llama = self.llama_proj(query_output.last_hidden_state)
+        atts_llama = torch.ones(inputs_llama.size()[:-1], dtype=torch.long).to(device)  # [B, 32]
 
         self.llama_tokenizer.padding_side = "left"
 
-        text = [t + "\n" for t in samples["text_input"]]
+        # text = [t + "\n" for t in samples["text_input"]]
+        text = [self.prompt + t + "\n" for t in samples["text_input"]]
 
         llama_tokens = self.llama_tokenizer(
             text,
@@ -135,20 +155,31 @@ class Blip2Llama(Blip2Base):
             max_length=self.max_txt_len,
         ).to(device)
 
+        # llama_tokenizer 会自动在输入的文本前面加上 bos token, 所以这里要减去1
+        llama_tokens.input_ids = llama_tokens.input_ids[:, 1:]
+        llama_tokens.attention_mask = llama_tokens.attention_mask[:, 1:]
+
+        # 把padding 的位置设置为 -100, 这样就不会计算loss
+        # targets 尺寸为 [B, N]
         targets = llama_tokens.input_ids.masked_fill(
             llama_tokens.input_ids == self.llama_tokenizer.pad_token_id, -100
         )
+
+        # 如果有一个固定的 prompt, 那么就把这个prompt对应的位置的  也设置为-100
+        # 有点疑惑, llama_tokens 是根据输入的文字生成的, 并不包含 self.prompt， 所以如果把前N个位置认为是self.prompt，就会导致后续出错
         if self.prompt:
             targets[:, : self.prompt_length] = -100  # do not apply loss to the prompt
 
+        # [B, 32]
         empty_targets = (
-            torch.ones(atts_opt.size(), dtype=torch.long).to(device).fill_(-100)
+            torch.ones(atts_llama.size(), dtype=torch.long).to(device).fill_(-100)
         )
+        # [B, 32] cat [B, N]  -> [B, 32+N]
         targets = torch.cat([empty_targets, targets], dim=1)
 
         inputs_embeds = self.llama_model.model.embed_tokens(llama_tokens.input_ids)
-        inputs_embeds = torch.cat([inputs_opt, inputs_embeds], dim=1)
-        attention_mask = torch.cat([atts_opt, llama_tokens.attention_mask], dim=1)
+        inputs_embeds = torch.cat([inputs_llama, inputs_embeds], dim=1)
+        attention_mask = torch.cat([atts_llama, llama_tokens.attention_mask], dim=1)
 
         with self.maybe_autocast():
             outputs = self.llama_model(
@@ -159,7 +190,11 @@ class Blip2Llama(Blip2Base):
             )
         loss = outputs.loss
 
-        return {"loss": loss}
+        # output_text = self.llama_tokenizer.decode(outputs.logits[0][self.num_query_token:].argmax(1))
+        output_text = self.llama_tokenizer.batch_decode(outputs.logits[:, self.num_query_token:].argmax(2), 
+                                                        skip_special_tokens=True, clean_up_tokenization_spaces=True)
+        # output_text 应该是一个长度等于 batch 的list
+        return {"loss": loss, "output_text": output_text}
 
     @torch.no_grad()
     def generate(
@@ -197,10 +232,10 @@ class Blip2Llama(Blip2Base):
         image = samples["cloud"]
         device = image["coord"].device
         with self.maybe_autocast():
-            fake_cloud_encoder_result = torch.rand(image["coord"].shape[0], 256, 384).to(device)        # [batch_size, 256, 384]
-            image_embeds = self.ln_cloud(fake_cloud_encoder_result)
+            # fake_cloud_encoder_result = torch.rand(image["coord"].shape[0], 256, 384).to(device)        # [batch_size, 256, 384]
+            # image_embeds = self.ln_cloud(fake_cloud_encoder_result)
 
-            # image_embeds = self.ln_cloud(self.cloud_encoder(image))
+            image_embeds = self.ln_cloud(self.cloud_encoder(image))
             image_atts = torch.ones(image_embeds.size()[:-1], dtype=torch.long).to(device)
 
             query_tokens = self.query_tokens.expand(image_embeds.shape[0], -1, -1)
@@ -211,7 +246,7 @@ class Blip2Llama(Blip2Base):
                 return_dict=True,
             )
 
-            inputs_opt = self.opt_proj(query_output.last_hidden_state)
+            inputs_opt = self.llama_proj(query_output.last_hidden_state)
             atts_opt = torch.ones(inputs_opt.size()[:-1], dtype=torch.long).to(device)
 
             if "text_input" in samples.keys():
@@ -222,47 +257,47 @@ class Blip2Llama(Blip2Base):
             prompt = [prompt] * image_embeds.size(0)
 
             llama_tokens = self.llama_tokenizer(prompt, return_tensors="pt").to(device)
-
-            inputs_embeds = self.llama_model.model.embed_tokens(llama_tokens.input_ids)
-            inputs_embeds = torch.cat([inputs_opt, inputs_embeds], dim=1)
+            input_embeds = self.llama_model.model.embed_tokens(llama_tokens.input_ids)
+            input_embeds = torch.cat([inputs_opt, input_embeds], dim=1)         # 把 learnable query 和 prompt embedding 结果拼接起来
             attention_mask = torch.cat([atts_opt, llama_tokens.attention_mask], dim=1)
 
             if use_nucleus_sampling:
-                query_embeds = inputs_embeds.repeat_interleave(num_captions, dim=0)
+                query_embeds = input_embeds.repeat_interleave(num_captions, dim=0)
                 num_beams = 1
             else:
-                query_embeds = inputs_embeds.repeat_interleave(num_beams, dim=0)
+                query_embeds = input_embeds.repeat_interleave(num_beams, dim=0)
 
-            generated_ids = self.llama_model.generate(
-                llama_tokens.input_ids,
-                inputs_embeds=query_embeds,
-                attention_mask=attention_mask,
-                max_new_tokens=max_length,
-                do_sample = use_nucleus_sampling,
-                num_beams = num_beams,
-                top_k = top_k,
-                top_p = top_p,
-                temperature = temperature,
-                no_repeat_ngram_size = no_repeat_ngram_size,
-            )
-            results = self.llama_tokenizer.batch_decode(generated_ids, skip_special_tokens=True, spaces_between_special_tokens=False)
-            return results
 
-            # outputs = self.llama_model.generate(
-            #     input_ids=input_ids,
+            # generated_ids = self.llama_model.generate(
+            #     llama_tokens.input_ids,
             #     inputs_embeds=query_embeds,
             #     attention_mask=attention_mask,
-            #     do_sample=use_nucleus_sampling,
-            #     top_p=top_p,
-            #     temperature=temperature,
-            #     num_beams=num_beams,
             #     max_new_tokens=max_length,
-            #     min_length=min_length,
-            #     eos_token_id=self.eos_token_id,
-            #     repetition_penalty=repetition_penalty,
-            #     length_penalty=length_penalty,
-            #     num_return_sequences=num_captions,
+            #     do_sample = use_nucleus_sampling,
+            #     num_beams = num_beams,
+            #     top_k = top_k,
+            #     top_p = top_p,
+            #     temperature = temperature,
+            #     no_repeat_ngram_size = no_repeat_ngram_size,
             # )
+            # results = self.llama_tokenizer.batch_decode(generated_ids, skip_special_tokens=True, spaces_between_special_tokens=False)
+            # return results
+
+            outputs = self.llama_model.generate(
+                # input_ids=input_ids,
+                inputs_embeds=query_embeds,
+                attention_mask=attention_mask,
+                do_sample=use_nucleus_sampling,
+                top_p=top_p,
+                temperature=temperature,
+                num_beams=num_beams,
+                max_new_tokens=max_length,
+                min_length=min_length,
+                eos_token_id=self.eos_token_id,
+                repetition_penalty=repetition_penalty,
+                length_penalty=length_penalty,
+                num_return_sequences=num_captions,
+            )
 
             prompt_length = llama_tokens.input_ids.shape[1]
             output_text = self.llama_tokenizer.batch_decode(
@@ -270,6 +305,120 @@ class Blip2Llama(Blip2Base):
             )
             output_text = [text.strip() for text in output_text]
             return output_text
+
+
+    @torch.no_grad()
+    def generate_with_hidden_prompt(
+        self,
+        samples,
+        use_nucleus_sampling=False,
+        num_beams=1,
+        max_length=30,
+        min_length=1,
+        top_p=0.95,
+        top_k=50,
+        no_repeat_ngram_size=6,
+        repetition_penalty=1.0,
+        length_penalty=1.0,
+        num_captions=1,
+        temperature=0.7,
+    ):
+        front_prompt = "以下是一个描述任务的指令，请写一个完成该指令的适当回复。\n\n ### 指令:\n"
+        end_prompt = "\n\n### 回复:"
+        image = samples["cloud"]
+        device = image["coord"].device
+        with self.maybe_autocast():
+            # fake_cloud_encoder_result = torch.rand(image["coord"].shape[0], 256, 384).to(device)        # [batch_size, 256, 384]
+            # image_embeds = self.ln_cloud(fake_cloud_encoder_result)
+
+            image_embeds = self.ln_cloud(self.cloud_encoder(image))
+            image_atts = torch.ones(image_embeds.size()[:-1], dtype=torch.long).to(device)
+
+            query_tokens = self.query_tokens.expand(image_embeds.shape[0], -1, -1)
+            query_output = self.Qformer.bert(
+                query_embeds=query_tokens,
+                encoder_hidden_states=image_embeds,
+                encoder_attention_mask=image_atts,
+                return_dict=True,
+            )
+    
+            # Q-Former 的 learnable query 映射到 llama 的特征空间后得到的特征以及 attention mask
+            inputs_query = self.llama_proj(query_output.last_hidden_state)
+            atts_query = torch.ones(inputs_query.size()[:-1], dtype=torch.long).to(device)
+
+            if "text_input" in samples.keys():
+                prompt = samples["text_input"]
+            else:
+                prompt = self.prompt
+
+            # 对起始的token进行处理
+            front_tokens = self.llama_tokenizer(front_prompt, return_tensors="pt").to(device)
+            front_embeds = self.llama_model.model.embed_tokens(front_tokens.input_ids)
+
+            # 对输入的文字prompt进行处理，因为每个prompt都会添加一个 bos token, 所以需要把bos token去掉
+            prompt = [prompt] * image_embeds.size(0)
+            prompt_tokens = self.llama_tokenizer(prompt, return_tensors="pt").to(device)
+            prompt_tokens.input_ids = prompt_tokens.input_ids[:, 1:]
+            prompt_tokens.attention_mask = prompt_tokens.attention_mask[:, 1:]
+            prompt_embeds = self.llama_model.model.embed_tokens(prompt_tokens.input_ids)
+            
+
+            # 对于结尾的 token 需要额外处理, 因为每个prompt都会添加一个 bos token, 而结尾的prompt要和前面的tokenizer结果拼接起来
+            # 所以需要把结尾的prompt的bos token去掉
+            end_tokens = self.llama_tokenizer(end_prompt, return_tensors="pt").to(device)
+            end_tokens.input_ids = end_tokens.input_ids[:, 1:]
+            end_tokens.attention_mask = end_tokens.attention_mask[:, 1:]
+            end_embeds = self.llama_model.model.embed_tokens(end_tokens.input_ids)
+
+            # 最后进行拼接  front + query + prompt + end
+            input_embeds = torch.cat([front_embeds, inputs_query, prompt_embeds, end_embeds], dim=1)
+            attention_mask = torch.cat([front_tokens.attention_mask, atts_query, prompt_tokens.attention_mask, end_tokens.attention_mask], dim=1)
+
+
+            if use_nucleus_sampling:
+                input_embeds = input_embeds.repeat_interleave(num_captions, dim=0)
+                num_beams = 1
+            else:
+                input_embeds = input_embeds.repeat_interleave(num_beams, dim=0)
+
+
+            # generated_ids = self.llama_model.generate(
+            #     llama_tokens.input_ids,
+            #     inputs_embeds=query_embeds,
+            #     attention_mask=attention_mask,
+            #     max_new_tokens=max_length,
+            #     do_sample = use_nucleus_sampling,
+            #     num_beams = num_beams,
+            #     top_k = top_k,
+            #     top_p = top_p,
+            #     temperature = temperature,
+            #     no_repeat_ngram_size = no_repeat_ngram_size,
+            # )
+            # results = self.llama_tokenizer.batch_decode(generated_ids, skip_special_tokens=True, spaces_between_special_tokens=False)
+            # return results
+
+            outputs = self.llama_model.generate(
+                # input_ids=input_ids,
+                inputs_embeds=input_embeds,
+                attention_mask=attention_mask,
+                do_sample=use_nucleus_sampling,
+                top_p=top_p,
+                temperature=temperature,
+                num_beams=num_beams,
+                max_new_tokens=max_length,
+                min_length=min_length,
+                eos_token_id=self.eos_token_id,
+                repetition_penalty=repetition_penalty,
+                length_penalty=length_penalty,
+                num_return_sequences=num_captions,
+            )
+
+            output_text = self.llama_tokenizer.batch_decode(
+                outputs, skip_special_tokens=True, spaces_between_special_tokens=False
+            )
+            output_text = [text.strip() for text in output_text]
+            return output_text
+        
 
     @classmethod
     def from_config(cls, cfg):
